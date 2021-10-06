27cf9b70440d6e52c1888e6b63cf09c850f95d62
Orange <bobrov@ttfb.dev>
2021-10-06 21:40:00 +0300

    upd

--- app/Http/Controllers/BattleController.php
+++ app/Http/Controllers/BattleController.php
@@ -147,7 +147,7 @@ class BattleController extends Controller
         return response()->json([
             'log' => FightLog::read(),
             'status' => $battle->getStatus(),
-            'winner' => $winner,
+            'winner' => $winner ?? '',
         ]);
     }
 }
