d5be91bb1122ac819392861b91e983cbc672d56e
Orange <bobrov@ttfb.dev>
2021-10-11 01:47:40 +0300

    upd

--- app/Battle/Modules/Weapon/RocketModule.php
+++ app/Battle/Modules/Weapon/RocketModule.php
@@ -4,7 +4,7 @@
 namespace App\Battle\Modules\Weapon;
 
 
-use App\Battle\Actions\DamageHealthAction;
+use App\Battle\Actions\DamageEnergyAction;
 use App\Battle\Battle;
 use App\Battle\Member;
 use App\Battle\Modules\BaseModule;
@@ -14,7 +14,7 @@ class RocketModule extends BaseModule
     public function getActions(Battle $battle, Member $action_owner, Member $round_opponent): array
     {
         return [
-            (new DamageHealthAction($action_owner, $round_opponent, $battle->getFightRoundNumber(), $this->getId()))->setDamage($this->getDamage())
+            (new DamageEnergyAction($action_owner, $round_opponent, $battle->getFightRoundNumber(), $this->getId()))->setDamage($this->getDamage())
         ];
     }
 }
