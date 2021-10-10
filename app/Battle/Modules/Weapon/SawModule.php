4a1c48ee160c4c3dda4a7d02d4130f2c0021f48c
Orange <bobrov@ttfb.dev>
2021-10-11 02:01:07 +0300

    upd

--- app/Battle/Modules/Weapon/SawModule.php
+++ app/Battle/Modules/Weapon/SawModule.php
@@ -4,7 +4,7 @@
 namespace App\Battle\Modules\Weapon;
 
 
-use App\Battle\Actions\DamageHealthAction;
+use App\Battle\Actions\DamageEnergyAction;
 use App\Battle\Battle;
 use App\Battle\Member;
 use App\Battle\Modules\BaseModule;
@@ -14,8 +14,8 @@ class SawModule extends BaseModule
     public function getActions(Battle $battle, Member $action_owner, Member $round_opponent): array
     {
         return [
-            (new DamageHealthAction($action_owner, $round_opponent, $battle->getFightRoundNumber(), $this->getId()))->setDamage($this->getDamage()),
-            (new DamageHealthAction($action_owner, $round_opponent, $battle->getFightRoundNumber(), $this->getId()))->setDamage($this->getDamage()),
+            (new DamageEnergyAction($action_owner, $round_opponent, $battle->getFightRoundNumber(), $this->getId()))->setDamage($this->getDamage()),
+            (new DamageEnergyAction($action_owner, $round_opponent, $battle->getFightRoundNumber(), $this->getId()))->setDamage($this->getDamage()),
         ];
     }
 }
